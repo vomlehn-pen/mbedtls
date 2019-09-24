91875acc01d6cded45092119602e923d23911795
David VomLehn <vomlehn@pensando.io>
2019-09-23 19:12:05 -0700

    Turn off parameter checking.

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -297,7 +297,7 @@
  *
  * Uncomment to enable validation of application-controlled parameters.
  */
-#define MBEDTLS_CHECK_PARAMS
+//#define MBEDTLS_CHECK_PARAMS
 
 /* \} name SECTION: System support */
 
